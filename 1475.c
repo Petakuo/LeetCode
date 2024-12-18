2dd39c28455851f9c71ee53c5b6b974e461472fc
Petakuo <162006888+Petakuo@users.noreply.github.com>
2024-12-18 11:09:21 +0800

    Update 1475.c
    Final Prices With a Special Discount in a Shop (E)

--- 1475.c
+++ 1475.c
@@ -17,3 +17,28 @@ int* finalPrices(int* prices, int pricesSize, int* returnSize) {
     }
     return ans;
 }
+
+Sol2: 
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* finalPrices(int* prices, int pricesSize, int* returnSize) {
+    *returnSize = pricesSize;
+    int* ans = (int*)malloc(pricesSize*sizeof(int));
+    int* stack = (int*)malloc(pricesSize*sizeof(int));
+    int top = -1;
+    for(int i = 0; i < pricesSize; i++){
+        while(top >= 0 && prices[i] <= prices[stack[top]]){
+            ans[stack[top]] = prices[stack[top]]-prices[i];
+            top--;
+        }
+        top++;
+        stack[top] = i;
+    }
+    while(top >= 0){
+        ans[stack[top]] = prices[stack[top]];
+        top--;
+    }
+    free(stack);
+    return ans;
+}
