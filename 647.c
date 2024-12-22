7d3cc4677b0b02c2091fef0239d4d37618e9b7fb
Petakuo <162006888+Petakuo@users.noreply.github.com>
2024-12-22 22:27:26 +0800

    Update 647.c
    Palindromic Substrings (M)

--- 647.c
+++ 647.c
@@ -33,3 +33,31 @@ int countSubstrings(char* s) {
     }
     return ans;
 }
+
+Sol3: 
+int countSubstrings(char* s) {
+    int len = strlen(s);
+    int ans = 0;
+    bool dp[len][len];
+    memset(dp, 0, sizeof(dp));
+    for(int i = 0; i < len; i++){
+        dp[i][i] = 1;
+        ans++;
+    }
+    for(int i = 0; i < len-1; i++){
+        if(s[i] == s[i+1]){
+            dp[i][i+1] = 1;
+            ans++;
+        }
+    }
+    for(int length = 3; length <= len; length++){
+        for(int i = 0; i <= len-length; i++){
+            int j = i+length-1;
+            if((s[i] == s[j]) && (dp[i+1][j-1] == 1)){
+                dp[i][j] = 1;
+                ans++;
+            }
+        }
+    }
+    return ans;
+}
