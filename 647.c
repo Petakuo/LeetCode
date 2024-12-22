649d29969db8292f97a5502668f323fc33eaf25a
Petakuo <162006888+Petakuo@users.noreply.github.com>
2024-12-22 21:39:27 +0800

    Update 647.c
    Palindromic Substrings (M)

--- 647.c
+++ 647.c
@@ -1,3 +1,4 @@
+Sol1: 
 bool IsPal(char* s, int left, int right){
     while(left < right){
         if(s[left++] != s[right--]) return 0;
@@ -12,6 +13,22 @@ int countSubstrings(char* s) {
         for(int j = 0; j < len-i+1; j++){
             int k = j+i-1;
             if(IsPal(s, j, k)) ans++;
+        }
+    }
+    return ans;
+}
+
+Sol2: 
+int countSubstrings(char* s) {
+    int len = strlen(s);
+    int ans = 0;
+    for(int i = 0; i < 2*len-1; i++){
+        int left = i/2;
+        int right = left+i%2;
+        while((left >= 0) && (right < len) && (s[left] == s[right])){
+            ans++;
+            left--;
+            right++;
         }
     }
     return ans;
