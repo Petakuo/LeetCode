92e92ed1f77db08410fe0a85bf4b8b9e7c79c66a
Petakuo <162006888+Petakuo@users.noreply.github.com>
2025-01-27 12:54:45 +0800

    Update 832.c

--- 832.c
+++ 832.c
@@ -1,4 +1,4 @@
-/**
+/*
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
