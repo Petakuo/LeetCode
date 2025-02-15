da177c865be89014208df2e657181cb3185ec7a5
Petakuo <162006888+Petakuo@users.noreply.github.com>
2025-02-15 14:33:27 +0800

    Update 1352.c
    Product of the Last K Numbers (M)

--- 1352.c
+++ 1352.c
@@ -1,6 +1,4 @@
-
-
-
+Sol1: 
 typedef struct {
     int* arr;
     int capacity;
@@ -34,6 +32,56 @@ int productOfNumbersGetProduct(ProductOfNumbers* obj, int k) {
 
 void productOfNumbersFree(ProductOfNumbers* obj) {
     free(obj->arr);
+    free(obj);
+}
+
+/**
+ * Your ProductOfNumbers struct will be instantiated and called as such:
+ * ProductOfNumbers* obj = productOfNumbersCreate();
+ * productOfNumbersAdd(obj, num);
+ 
+ * int param_2 = productOfNumbersGetProduct(obj, k);
+ 
+ * productOfNumbersFree(obj);
+*/
+
+Sol2: 
+typedef struct {
+    int* prefix;
+    int capacity;
+    int size;
+} ProductOfNumbers;
+
+
+ProductOfNumbers* productOfNumbersCreate() {
+    ProductOfNumbers* obj = (ProductOfNumbers*)malloc(sizeof(ProductOfNumbers));
+    obj->capacity = 4;
+    obj->prefix = (int*)malloc(obj->capacity*sizeof(int));
+    obj->size = 1;
+    obj->prefix[0] = 1;
+    return obj;
+}
+
+void productOfNumbersAdd(ProductOfNumbers* obj, int num) {
+    if(num == 0){
+        obj->size = 1;
+        obj->prefix[0] = 1;
+        return;
+    }
+    if(obj->size == obj->capacity){
+        obj->capacity *= 2;
+        obj->prefix = (int*)realloc(obj->prefix, obj->capacity*sizeof(int));
+    }
+    obj->prefix[obj->size++] = obj->prefix[obj->size-1]*num;
+}
+
+int productOfNumbersGetProduct(ProductOfNumbers* obj, int k) {
+    if(k >= obj->size) return 0;
+    else return obj->prefix[obj->size-1]/obj->prefix[obj->size-1-k];
+}
+
+void productOfNumbersFree(ProductOfNumbers* obj) {
+    free(obj->prefix);
     free(obj);
 }
 
