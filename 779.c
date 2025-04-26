546e483888459f126cdf62ca7e7a2fb0ddabbd5f
Petakuo <162006888+Petakuo@users.noreply.github.com>
2025-04-26 17:15:03 +0800

    Update 779.c
    K-th Symbol in Grammar (M)

--- 779.c
+++ 779.c
@@ -1,3 +1,4 @@
+Sol1: 
 int kthGrammar(int n, int k) {
     int len = 1;
     while(len < k){
@@ -12,4 +13,12 @@ int kthGrammar(int n, int k) {
         }
     }
     return flip%2;
+}
+
+Sol2: 
+int kthGrammar(int n, int k) {
+    if(n == 1) return 0;
+    int half = 1 << (n-2);
+    if(k > half) return !kthGrammar(n-1, k-half);
+    else return kthGrammar(n-1, k);
 }
