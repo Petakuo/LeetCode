bbc0771b7120de85ef1f4a512b90ce457bed8850
Petakuo <162006888+Petakuo@users.noreply.github.com>
2025-06-03 14:07:56 +0800

    Update 1298.c
    Maximum Candies You Can Get from Boxes (H)

--- 1298.c
+++ 1298.c
@@ -9,28 +9,25 @@ int maxCandies(int* status, int statusSize, int* candies, int candiesSize, int**
         hasBox[initialBoxes[i]] = 1;
     }
     while(front < rear){
-        for(int i = front; i < rear; i++){
-            int box = queue[i];
-            if(opened[box]) continue;
-            if(status[box] == 0 && !canOpen[box]) continue;
-            ans += candies[box];
-            opened[box] = 1;
-            for(int j = 0; j < keysColSize[box]; j++){
-                int k = keys[box][j];
-                if(!canOpen[k]){
-                    canOpen[k] = 1;
-                    if(hasBox[k] && !opened[k]) queue[rear++] = k;  
-                }
-            }
-            for(int j = 0; j < containedBoxesColSize[box]; j++){
-                int k = containedBoxes[box][j];
-                if(!hasBox[k]){
-                    hasBox[k] = 1;
-                    queue[rear++] = k;
-                }
+        int box = queue[front++];
+        if(opened[box]) continue;
+        if(status[box] == 0 && !canOpen[box]) continue;
+        ans += candies[box];
+        opened[box] = 1;
+        for(int j = 0; j < keysColSize[box]; j++){
+            int k = keys[box][j];
+            if(!canOpen[k]){
+                canOpen[k] = 1;
+                if(hasBox[k] && !opened[k]) queue[rear++] = k;
             }
         }
-        front++;
+        for(int j = 0; j < containedBoxesColSize[box]; j++){
+            int k = containedBoxes[box][j];
+            if(!hasBox[k]){
+                hasBox[k] = 1;
+                queue[rear++] = k;
+            }
+        }
     }
     return ans;
 }
