61a30e45d713767b2377602f46e52db74830a63d
Petakuo <162006888+Petakuo@users.noreply.github.com>
2025-10-15 12:04:38 +0800

    Update 3349.c
    Adjacent Increasing Subarrays Detection I (E)

--- 3349.c
+++ 3349.c
@@ -1,3 +1,4 @@
+Sol1: 
 bool hasIncreasingSubarrays(int* nums, int numsSize, int k) {
     int ans = 1;
     for(int i = 0; i < numsSize-2*k+1; i++){
@@ -14,4 +15,17 @@ bool hasIncreasingSubarrays(int* nums, int numsSize, int k) {
         if(ans == 1) return ans;
     }
     return ans;
+}
+
+Sol2: 
+bool hasIncreasingSubarrays(int* nums, int numsSize, int k) {
+    int inc[numsSize];
+    inc[numsSize-1] = 1;
+    for(int i = numsSize-2; i >= 0; i--){
+        inc[i] = nums[i] < nums[i+1] ? inc[i+1]+1 : 1;
+    }
+    for(int i = 0; i < numsSize-2*k+1; i++){
+        if(inc[i] >= k && inc[i+k] >= k) return 1;
+    }
+    return 0;
 }
